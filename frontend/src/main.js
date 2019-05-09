deaac35ef896b55a9328f53e887cb4a5cecac868
Theodoros Zarkopafilis Ntakouris <zarkopafilis@gmail.com>
2019-05-09 14:56:36 +0300

    devserver with python, with mock responses structure

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -8,7 +8,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import Vuex from 'vuex'
 import store from '@/store'
 
 import App from '@/App'
@@ -22,8 +21,6 @@ import VueTruncate from 'vue-truncate-filter'
 import configureAxios from '@/api/AxiosConfig'
 
 import underscore from 'vue-underscore'
-
-Vue.use(Vuex)
 
 Vue.use(BootstrapVue)
 Vue.use(VueTruncate)
