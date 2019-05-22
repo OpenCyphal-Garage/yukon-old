607fc20e8a0e3ee659ea8c7ad7c43ccd470dcc10
Theodoros Zarkopafilis Ntakouris <zarkopafilis@gmail.com>
2019-05-22 12:10:04 +0300

    Added forgotten licensing headers

--- frontend/src/util/usability.js
+++ frontend/src/util/usability.js
@@ -1,3 +1,10 @@
+/*
+ * Copyright (C) 2019 UAVCAN Development Team  <uavcan.org>
+ * This software is distributed under the terms of the MIT License.
+ *
+ * Author: Theodoros Ntakouris <zarkopafilis@gmail.com>
+ */
+
 export default function copyToClipboard (text) {
   // Create an element with position -9999 px, focus it's content, copy the content, remove it
   const el = document.createElement('textarea')
