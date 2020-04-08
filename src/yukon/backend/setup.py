22fe558f489da44bbccc62b4bf8328011e7ec134
TSC21 <n.marques21@hotmail.com>
2020-04-08 03:08:59 +0100

    backend: add setup.py

---
+++ src/yukon/backend/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2020  UAVCAN Development Team  <uavcan.org>
+#               2020  dronesolutions.io. All rights reserved.
+# This software is distributed under the terms of the MIT License.
+#
+
+import sys
+import setuptools
+
+from typing import Dict
+
+if int(setuptools.__version__.split('.')[0]) < 30:
+    print('A newer version of setuptools is required. The current version does not support declarative config.',
+          file=sys.stderr)
+    sys.exit(1)
+
+version = {}  # type: Dict
+with open('src/api/version.py') as fp:
+    exec(fp.read(), version)
+
+setuptools.setup(version=version['__version__'],
+                 package_data={'': ['*.ini']})
