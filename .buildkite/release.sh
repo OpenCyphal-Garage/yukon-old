24593713a310347d743a12a023cf9e7355ca1689
TSC21 <n.marques21@hotmail.com>
2020-04-12 21:18:58 +0100

    fix release versions and release process

--- .buildkite/release.sh
+++ .buildkite/release.sh
@@ -25,9 +25,16 @@ set -o pipefail
 
 # +----------------------------------------------------------+
 
-export YUKON_FULL_VERSION=$(grep __version__ src/yukon/backend/api/version.py | awk '{print $3}' | sed -E "s/'([0-9]+\.[0-9]+\.[0-9]+)'/\1/g")
-export YUKON_MAJOR_MINOR_VERSION=$(echo $YUKON_FULL_VERSION | sed -E "s/([0-9]+\.[0-9]+)\.[0-9]+/\1/g")
+# Get the Backend and Frontend versions
+export YUKON_BACKEND_FULL_VERSION=$(grep __version__ src/yukon/backend/src/api/version.py | sed -E "s/'([0-9]+\.[0-9]+\.[0-9]+)'/\1/g")
+export YUKON_FRONTEND_FULL_VERSION=$(node -e "console.log(require('./src/yukon/frontend/package.json').version);")
+export YUKON_BACKEND_MAJOR_MINOR_VERSION=$(echo $YUKON_BACKEND_FULL_VERSION | sed -E "s/([0-9]+\.[0-9]+)\.[0-9]+/\1/g")
+export YUKON_FRONTEND_MAJOR_MINOR_VERSION=$(echo $YUKON_FRONTEND_FULL_VERSION | sed -E "s/([0-9]+\.[0-9]+)\.[0-9]+/\1/g")
 tox
 tox -e sonar | grep -v "sonar.login"
-tox -e upload | grep -v "twine upload"
-tox -e upload | grep -v "npm publish"
+# Upload yukon_backend to PyPi
+tox -e pypi_upload | grep -v "twine upload"
+# Only publish yukon_frontend to npm if the version is different from the one already published (otherwise it will fail)
+if [[ $YUKON_FRONTEND_FULL_VERSION != $(npm view yukon_frontend version) ]]; then
+  tox -e npm_publish | grep -v "npm publish"
+fi
