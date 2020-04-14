414ffdc12f1b3d8b695ad21d1ca08bd6a7b222f9
TSC21 <n.marques21@hotmail.com>
2020-04-14 18:46:33 +0100

    backend: add working bus monitor which extracts info of nodes based on the heartbeats received

--- src/yukon/backend/src/api/app.py
+++ src/yukon/backend/src/api/app.py
@@ -9,27 +9,37 @@
 # App, Static, Templates folder, CORS setup
 # Controller wiring
 
+import quart
 from quart import Quart, render_template
 from quart_cors import cors
 # Controllers
 from controllers.nodes import nodes_controller
+from controllers.nodes import MonitorNode
 
-api_prefix = '/api/v1'
-
-app = Quart(__name__,
-            static_folder='../../../frontend/static/',
-            template_folder='../../../frontend/')
-app = cors(app)
-
-# Register endpoint modules
-app.register_blueprint(nodes_controller, url_prefix=api_prefix + '/nodes')
-
-
-# Sink all undeclared routes so that vue can work with router properly
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-async def catch_all(path: str) -> str:
-    return await render_template('index.html')
+import asyncio
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    loop = asyncio.get_event_loop()
+    monitor_node = MonitorNode(loop)
+
+    app = Quart(__name__,
+                static_folder='../../../frontend/static/',
+                template_folder='../../../frontend/')
+    app = cors(app)
+
+    api_prefix = '/api/v1'
+
+    # Register endpoint modules
+    app.register_blueprint(nodes_controller, url_prefix=api_prefix + '/nodes')
+
+    # Sink all undeclared routes so that vue can work with router properly
+    @app.route('/', defaults={'path': ''})
+    @app.route('/<path:path>')
+    async def catch_all(path: str) -> str:
+        return await render_template('index.html')
+
+    loop.run_until_complete(app.run_task(port=5000))
+    loop.close()
+
+
+
