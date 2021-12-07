24f521ae2d133c44ea55db3cc5e32ec37fe9553f
Pavel Kirienko <pavel.kirienko@gmail.com>
2021-12-07 16:02:30 +0200

    Finalize project reorganization; nox next

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@
 # Author: Pavel Kirienko <pavel@uavcan.org>
 # type: ignore
 
+import toml
 import setuptools
 import distutils.command.build_py
 from pathlib import Path
@@ -11,8 +12,8 @@ from pathlib import Path
 
 PACKAGE_NAME = "yukon"
 PROJECT_ROOT = Path(__file__).resolve().parent
-DSDL_SOURCE_ROOT = PROJECT_ROOT / PACKAGE_NAME / "dsdl_src"
 
+meta = toml.load(PROJECT_ROOT / "pyproject.toml")
 
 # noinspection PyUnresolvedReferences
 class BuildPy(distutils.command.build_py.build_py):
@@ -22,8 +23,8 @@ class BuildPy(distutils.command.build_py.build_py):
 
             compile_all(
                 [
-                    DSDL_SOURCE_ROOT / "public_regulated_data_types" / "uavcan",
-                    DSDL_SOURCE_ROOT / "public_unregulated_data_types" / "org_uavcan_yukon",
+                    PROJECT_ROOT / "deps" / "public_regulated_data_types" / "uavcan",
+                    PROJECT_ROOT / "dsdl" / "org_uavcan_yukon",
                 ],
                 Path(self.build_lib, PACKAGE_NAME, ".compiled").resolve(),
             )
@@ -33,6 +34,9 @@ class BuildPy(distutils.command.build_py.build_py):
 if __name__ == "__main__":
     # noinspection PyTypeChecker
     setuptools.setup(
+        name=meta["project"]["name"],
+        version=meta["project"]["version"],
+        description=meta["project"]["description"],
         cmdclass={"build_py": BuildPy},
         packages=setuptools.find_packages(str(PROJECT_ROOT), include=("yukon", "yukon.*")),
         package_data={"yukon": ["*"]},
