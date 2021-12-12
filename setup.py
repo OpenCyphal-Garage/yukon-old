9ffb6add442a7c6a50e1e9b5a1d9103d374a4a65
Pavel Kirienko <pavel.kirienko@gmail.com>
2021-12-12 03:43:25 +0200

    Implement lazy DSDL compilation to speed up testing

--- setup.py
+++ setup.py
@@ -12,21 +12,26 @@ from pathlib import Path
 PACKAGE_NAME = "yukon"
 PROJECT_ROOT = Path(__file__).resolve().parent
 COMPILED_DIR = PROJECT_ROOT / PACKAGE_NAME / "_compiled"
+DSDL_NAMESPACES = [
+    PROJECT_ROOT / "deps" / "public_regulated_data_types" / "uavcan",
+    PROJECT_ROOT / "dsdl" / "org_uavcan_yukon",
+]
 
 
 # noinspection PyUnresolvedReferences
 class BuildPy(distutils.command.build_py.build_py):
     def run(self):
         if not self.dry_run:
-            from pyuavcan.dsdl import compile_all
+            ts_input = max(x.stat().st_mtime for ns in DSDL_NAMESPACES for x in ns.rglob("*.uavcan"))
+            ts_output = COMPILED_DIR.stat().st_mtime if COMPILED_DIR.is_dir() else 0.0
+            if ts_input > ts_output:
+                from pyuavcan.dsdl import compile_all
 
-            compile_all(
-                [
-                    PROJECT_ROOT / "deps" / "public_regulated_data_types" / "uavcan",
-                    PROJECT_ROOT / "dsdl" / "org_uavcan_yukon",
-                ],
-                COMPILED_DIR,
-            )
+                print("Compiling DSDL:", list(map(str, DSDL_NAMESPACES)), " --> ", COMPILED_DIR)
+                compile_all(DSDL_NAMESPACES, COMPILED_DIR)
+                COMPILED_DIR.touch(exist_ok=True)
+            else:
+                print("DSDL compilation skipped: DSDL sources not modified")
         super().run()
 
 
